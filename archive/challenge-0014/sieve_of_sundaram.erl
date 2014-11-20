1e3acd8770975cce92e9a0fce2729631d923f0dc
psyomn <lethaljellybean@gmail.com>
2014-11-20 05:14:28 -0500

    challenge 14: intermediate solution

---
+++ archive/challenge-0014/sieve_of_sundaram.erl
@@ -0,0 +1,26 @@
+-module(sieve_of_sundaram).
+-author("lethaljellybean@gmail.com").
+-export([main/1]).
+-import(lists, [seq/2, usort/1]).
+
+%% @doc don't care about input
+main(_) ->
+  Max      = round(math:sqrt(1000)),
+  Unwanted = usort(make_sequence(Max)),
+  Wanted   = seq(1,1000) -- Unwanted,
+  Final    = make_odd_primes(Wanted),
+  io:format("~p~n", [Final]).
+
+%% @doc according to the sieve, after w're done collecting the numbers we don't
+%%   want,
+make_odd_primes(L) ->
+  [X * 2 + 1 || X <- L].
+
+%% @doc calculate the unwanted numbers in the form of i + j + 2ij <= n
+make_sequence(Max) ->
+  [num_calc(X, Y) || X <- seq(1, Max),
+                     Y <- seq(1, Max),
+                     num_calc(X, Y) =< Max].
+
+num_calc(X, Y) ->
+  X + Y + 2 * X * Y.
