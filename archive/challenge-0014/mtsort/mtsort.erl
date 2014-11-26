4824c371031b9db6febe6bfdd0d5e27d1927da02
psyomn <lethaljellybean@gmail.com>
2014-11-26 00:46:03 -0500

    challenge 14: segment sorting works

--- archive/challenge-0014/mtsort/mtsort.erl
+++ archive/challenge-0014/mtsort/mtsort.erl
@@ -1,16 +1,19 @@
 -module(mtsort).
 -author("lethaljellybean@gmail.com").
+-import(nice_chunk, [nice_chunk/2]).
 -compile(export_all).
+-define(NUMRANGE, 1000000).
 
 %% @doc challenge 14, hard; make millions of numbers. Make thread, and sort the
 %% numbers.
 
 main(NumThreads) ->
   SanitizedArgs = sanitize(NumThreads),
-  Data    = make_random_list(),
 
-  Chunked     = n_chunks(Data, SanitizedArgs),
-  MasterPID   = spawn(mtsort, master_process, [SanitizedArgs, []]),
+  Data = make_random_list(),
+
+  Chunked     = nice_chunk(SanitizedArgs, Data),
+  MasterPID   = spawn(?MODULE, master_process, [SanitizedArgs, []]),
   Workers     = make_workers(SanitizedArgs),
   print_workers(Workers),
 
@@ -18,9 +21,7 @@ main(NumThreads) ->
   lists:map(fun(X) -> {W, C} = X, W ! {C, MasterPID} end, Workload).
 
 master_process(0,   Acc) ->
-  %% TODO merge logic should go here
   io:format("Final answer is: ~p~n", [Acc]);
-
 master_process(RespCount, Acc) ->
   receive
     L ->
@@ -33,63 +34,35 @@ merge() -> todo.
 process_each_chunk(ListChunks, WorkerPIDs) -> todo.
 
 %% @doc random number generation for testing with algorithm
-make_random_list()   -> make_random_list(30).
+make_random_list()   -> make_random_list(?NUMRANGE).
 make_random_list(Size) ->
   lists:map(fun(_) -> random_num() end, lists:seq(1,Size)).
 
-random_num() -> round(random:uniform() * 100).
+random_num() -> round(random:uniform() * 1000000).
 
 %% @doc return a list of PIDs of spawned worker processes
 make_workers(Num) when Num =:= 0 -> [];
 make_workers(Num) ->
-  [spawn(mtsort, worker, []) | make_workers(Num - 1)].
+  [spawn(?MODULE, worker, []) | make_workers(Num - 1)].
 
 print_workers(Workers) ->
   io:format("Workers: ~n"),
   lists:map(fun(X) -> io:format(" * ~p~n", [X]) end, Workers).
 
 % @doc Sanitize input from command line. Min threads, 1.
-sanitize(Args) when erlang:length(Args) =:= 0 -> 1;
+sanitize(Args) when length(Args) =:= 0 -> 1;
 sanitize(Args) ->
   [First|_] = Args,
   {K, _} = string:to_integer(First),
-  K.
+   K.
 
 %% @doc basic worker process that sorts a list (in our case a segment of a
 %% list).
 worker() ->
   receive
     {List, Other} ->
+      io:format("work received~n"),
       Sorted = lists:sort(List),
       Other  ! Sorted
-  end.
-
-%% @doc split list L into N chunks
-n_chunks([],_) -> [];
-n_chunks(L, 1) -> [L]; %% NB: list in list
-n_chunks(L, NumPartitions) ->
-  ChunkSize = ceiling(erlang:length(L) / NumPartitions),
-  n_chunks_by_size(L, ChunkSize).
-
-n_chunks_by_size(L, ChunkSize) when length(L) =< ChunkSize -> [L];
-n_chunks_by_size([], _)  -> [];
-n_chunks_by_size(L, ChunkSize) ->
-  [lists:sublist(L, 1, ChunkSize)] ++
-  n_chunks_by_size(
-    lists:sublist(L,
-    ChunkSize + 1,
-    erlang:length(L)),
-    ChunkSize).
-
-length_of_chunk(L, N) ->
-  X = erlang:length(L) / N,
-  round(X).
-
-ceiling(X) ->
-  T = erlang:trunc(X),
-  case (X - T) of
-    Neg when Neg < 0 -> T;
-    Pos when Pos > 0 -> T + 1;
-    _    -> T
   end.
 
