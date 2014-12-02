d6df1f8f0b86201e42c84fc58e611e2121b5a502
psyomn <lethaljellybean@gmail.com>
2014-12-02 00:03:21 -0500

    challenge 15: intermediate; almost done in rust

--- archive/challenge-0015/text_align/src/main.rs
+++ archive/challenge-0015/text_align/src/main.rs
@@ -1,5 +1,7 @@
 use std::io::File;
 
+/// Iterate through all the lines in a text chunk, and find the max characters
+/// found on a line.
 fn max_line_size(v: &Vec<u8>) -> u64 {
     let mut count: u64 = 0;
     let mut max: u64 = 0;
@@ -18,8 +20,28 @@ fn max_line_size(v: &Vec<u8>) -> u64 {
     return max;
 }
 
-fn main() {
-    let f_res = File::open(&Path::new("textfile.txt")).read_to_end();
-    let res: Vec<u8>  = f_res.ok().unwrap(); /* TODO how do we handle errors? */
+/// Depending on the line size, we'll print a bunch of whitespace before
+/// printing the rest of the line.
+fn pad_line(line: &Vec<u8>, max: u64) {
+    let line_size = line.len() as u64;
+    let diff = max - line_size;
 
+    if diff > 0 {
+        for x in range(1, diff) {
+            print!(" ");
+        }
+    }
+
+    print!("{}", line);
+}
+
+fn main() {
+    let f_res        = File::open(&Path::new("textfile.txt")).read_to_end();
+    let res: Vec<u8> = f_res.ok().unwrap(); /* TODO how do we handle errors? */
+    let max: u64     = max_line_size(&res);
+
+    for x in res.split(|a| (*a == '\n' as u8 || *a == '\r' as u8) ) {
+        let curr_line: Vec<u8> = (*x).iter().map(|&x| x).collect();
+        pad_line(&curr_line, max);
+    }
 }
