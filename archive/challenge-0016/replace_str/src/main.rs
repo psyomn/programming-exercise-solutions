0ffdf065573b6f31c657d57df6e2bcb7eadb34f0
psyomn <lethaljellybean@gmail.com>
2014-12-04 18:59:36 -0500

    challenge 16: easy; clean up solution with rust

--- archive/challenge-0016/replace_str/src/main.rs
+++ archive/challenge-0016/replace_str/src/main.rs
@@ -3,21 +3,23 @@ use std::os;
 fn main() {
     let vals: Vec<String> = os::args();
 
-    if (vals.len() < 3) {
+    if vals.len() < 3 {
         /* Make sure that we get anough parameters; rest is ignored */
         println!("Usage:");
         println!("  replace_str <main-string> <replace-char>+");
         return;
     }
 
-    let data: String = vals[1].clone(); // vals[0] is program name
+    let data: String = vals[1].clone(); 
     let to_rm: String = vals[2].clone();
 
-    let test = vals[2].as_slice().contains("a");
     let mut result_iter = data.as_slice().graphemes(true).filter(|a| !to_rm.as_slice().contains(*a));
+    let mut result : String = "".to_string();
 
     for x in result_iter {
-        print!("{}", x);
+        result.push_str(x);
     }
-    println!("");
+
+    println!("{}", result);
+
 }
