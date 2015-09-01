6eb8a21c448bafedbdb9b2d45f6f923157810d0b
psyomn <lethaljellybean@gmail.com>
2015-08-31 21:27:19 -0400

    challenge 17: intermediate: can check for presence

--- archive/challenge-0017/wsearch.erl
+++ archive/challenge-0017/wsearch.erl
@@ -1,14 +1,26 @@
 -module(wsearch).
 -author("lethaljellybean@gmail.com").
--export([main/1,request/1]).
+-export([main/1,request/1,contains_string/2]).
 
 main([]) ->
   io:format("Usage:~n  wsearch <webpage> <sentence>~n");
 main([Webpage,Sentence|_]) ->
-  io:format("Webpage to search on: ~p", [Webpage]),
-  io:format("Term to look for:     ~p", [Sentence]),
-  request(Webpage).
+  application:start(inets),
+  io:format("Webpage to search on: ~p~n", [Webpage]),
+  io:format("Term to look for:     ~p~n", [Sentence]),
+  Body = request(Webpage),
+  Found = contains_string(Body, Sentence),
+  io:format("Website contains sentence: ~p~n", [Found]).
 
+contains_string(Body, SearchPhrase) ->
+  LowerBody = string:to_lower(Body),
+  LowerPhrase = string:to_lower(SearchPhrase),
+  case string:str(LowerBody, LowerPhrase) of
+    0 -> false;
+    _ -> true
+  end.
+
+%% @doc request webpage, and return body contents
 request(Url) ->
-  {ok, {{Version, 200, ReasonPhrase}, Headers, Body}} = httpc:request(Url),
-  io:format("~p~n", [Body]).
+  {ok, {{_Version, 200, _ReasonPhrase}, _Headers, Body}} = httpc:request(Url),
+  Body.
