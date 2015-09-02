b6b2093be8803c6311bb3985cf85c0b172348c5b
psyomn <lethaljellybean@gmail.com>
2015-09-02 19:11:54 -0400

    challenge 17: hard: placing val works

--- archive/challenge-0017/tictac.erl
+++ archive/challenge-0017/tictac.erl
@@ -2,21 +2,38 @@
 -author("lethaljellybean@gmail.com").
 -export([main/1]).
 
-make_board() ->
-  [[b,b,b],
-   [b,b,b],
-   [b,b,b]].
-
 main(_) ->
   Board = make_board(),
-  step(Board),
-  io:format("Bye.~n").
+  BlankBoard = blankify_board(Board),
+  XBoard = place(1,1,BlankBoard,x),
+  print_board(XBoard).
 
-step(Board) ->
-  step(Board).
+make_board() ->
+  array:new(9).
 
-place(_Board, _X, _Y, V) when V /= x andalso V /= o ->
-  throw(not_x_or_o);
-place(Board, X, Y, V) ->
-  ToEdit = lists:nth(Y, Board).
+blankify_board(Board) ->
+  Size = array:size(Board),
+  blankify_board_back(Board, Size - 1).
 
+blankify_board_back(Board, -1) -> Board;
+blankify_board_back(Board, Size) ->
+  BNew = array:set(Size, b, Board),
+  blankify_board_back(BNew, Size - 1).
+
+print_board(Board) ->
+  print_board_back(Board, 0, array:size(Board)).
+
+print_board_back(_,     Max, Max) -> ok;
+print_board_back(Board, From, To) ->
+  io:format("~p ", [array:get(From, Board)]),
+  case (From + 1) rem 3 == 0 of
+    true -> io:format("~n");
+    false -> skip
+  end,
+  print_board_back(Board, From + 1, To).
+
+place(X, Y, Board, V) ->
+  array:set(Y * 3 + X, V, Board).
+
+get_at(X, Y, Board) ->
+  array:get(Y * 3 + X, Board).
