9538c0ffdd27f6c1e2bb1f05b466b6d657239564
psyomn <lethaljellybean@gmail.com>
2015-09-15 21:12:20 -0400

    challenge 18: hard: spiral, trivial additions

--- archive/challenge-0018/spiral.erl
+++ archive/challenge-0018/spiral.erl
@@ -4,7 +4,7 @@
 
 -include_lib("eunit/include/eunit.hrl").
 
--record(board, {data,currpos,curdir=down}).
+-record(board, {data,currpos,curdir=down,length=0}).
 
 main(_) ->
   CycleEven = spiral:new(10),
@@ -21,36 +21,42 @@ new(N) ->
   cyclezip(L, M, d).
 
 board_set_coord(B, X, Y) ->
-  B#board{data    = B#board.data,
-          currpos = {X, Y},
-          curdir  = B#board.curdir}.
+  B#board{currpos = {X, Y}}.
 
 board_set_curdir(B, Dir) ->
-  B#board{data    = B#board.data,
-          currpos = B#board.currpos,
-          curdir  = Dir}.
+  B#board{curdir  = Dir}.
 
 board_set_data(B, Data) ->
-  B#board{data    = Data,
-          currpos = B#board.currpos,
-          curdir  = B#board.curdir}.
+  B#board{data    = Data}.
+
+board_set_value(B, X, Y, V) ->
+  A  = B#board.data,
+  A2 = array:set(X * Y + X, V, A),
+  B#board{data=A2}.
+
+board_get_value(B, X, Y) ->
+  array:get(X * Y + X, B#board.data).
 
 %% @doc SpiralD are the directions zipped with the number sequence
 spiral_array(SpiralD) ->
   %% N is the largest direction ie. size of array
-  {N,_} = hd(SpiralD),
-  A = array:new(N*N),
-  Board = #board{data=A, currpos={0,N-1}, curdir=down}.
+  {N,_}  = hd(SpiralD),
+  A      = array:new(N*N),
+  Board  = #board{data=A, currpos={N-1,0}, curdir=down, length=N},
+  Spiral = spiral:make_lines(Board, SpiralD),
+  Spiral.
 
-make_line(Board,      []) -> Board;
-make_line(Board, SpiralD) ->
-  CurPos = Board#board.currpos,
-  case Board#board.curdir of
-    u -> todo;
-    d -> todo;
-    l -> todo;
-    r -> todo
-  end.
+make_lines(Board,      []) -> Board;
+make_lines(Board, SpiralD) ->
+  {CurX, CurY} = Board#board.currpos,
+  {N,Dir} = hd(SpiralD),
+  OffsetPos = CurX * CurY + CurX,
+  NewPos = case Board#board.curdir of
+             u -> {CurX, CurY - 1};
+             d -> {CurX, CurY + 1};
+             l -> {CurX - 1, CurY};
+             r -> {CurX + 1, CurY}
+           end.
 
 spiral_directions(N) when N rem 2 == 0 ->
   L = lists:reverse(lists:seq(3,N-1,2)),
@@ -87,20 +93,26 @@ new_test() ->
           spiral:new(11)).
 
 board_set_coord_test() ->
-  B = #board{data=array:new(10), currpos={1,1}, curdir=d},
+  B = #board{data=array:new(100), currpos={1,1}, curdir=d, length=10},
   C = board_set_coord(B, 4, 4),
   ?assert(C#board.currpos == {4, 4}).
 
 board_set_curdir_test() ->
-  B = #board{data=array:new(10), currpos={1,1}, curdir=d},
+  B = #board{data=array:new(100), currpos={1,1}, curdir=d, length=10},
   C = board_set_curdir(B, l),
   ?assert(C#board.curdir == l).
 
 board_set_data_test() ->
   B = #board{data=array:set(3,3, array:new(10)), currpos={1,1}, curdir=d},
   C = board_set_data(B, array:set(4,4,array:new(11))),
-  ?assert(B#board.data == array:set(3,3,array:new(10))),
+  ?assert(B#board.data    == array:set(3,3,array:new(10))),
   ?assert(C#board.data    == array:set(4,4,array:new(11))),
   ?assert(C#board.currpos == B#board.currpos),
   ?assert(C#board.curdir  == B#board.curdir).
+
+board_set_value_test() ->
+  B  = #board{data=array:set(3,3, array:new(10)), currpos={1,1}, curdir=d},
+  B2 = board_set_value(B,3,3,x),
+  V  = board_get_value(3,3,B2),
+  ?assert(V == x).
 
