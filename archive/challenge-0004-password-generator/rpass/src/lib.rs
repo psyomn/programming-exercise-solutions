74701ede1aa1651767406892fbf48e86a94298d1
psyomn <lethaljellybean@gmail.com>
2015-09-23 19:12:21 -0400

    challenge 4: rpass, rust, almost complete

--- archive/challenge-0004-password-generator/rpass/src/lib.rs
+++ archive/challenge-0004-password-generator/rpass/src/lib.rs
@@ -1,6 +1,24 @@
-fn random_password(x: u32, y: u32) -> String {
+extern crate rand;
+
+use rand::Rng;
+
+pub fn random_password(x: u32, y: u32) -> String {
+    let mut rng = rand::thread_rng();
+    let xusize = x as usize;
+    let mut c : String = String::new();
+
+    for _ in 0..y {
+        c = c + &rng
+            .gen_ascii_chars()
+            .take(xusize)
+            .collect::<String>()
+            + "\n";
+    }
+
+    c
 }
 
 #[test]
 fn it_works() {
+    assert_eq!(0, 1);
 }
