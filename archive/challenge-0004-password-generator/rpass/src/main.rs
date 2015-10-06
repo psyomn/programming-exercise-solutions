5dc2ddcbb66d63b0aa0fd7d1ec61d79cbc1bca9a
psyomn <lethaljellybean@gmail.com>
2015-10-06 11:11:21 -0400

    challenge 4: rpass, rust, add execution part

--- archive/challenge-0004-password-generator/rpass/src/main.rs
+++ archive/challenge-0004-password-generator/rpass/src/main.rs
@@ -1,4 +1,32 @@
+extern crate rpass;
+extern crate getopts;
+
+use std::env;
+use getopts::Options;
+
+use rpass::{random_password};
 
 fn main() {
-    println!("Hello world!");
+    let mut ar: Vec<String> = env::args().collect();
+
+    if ar.len() != 3 {
+        println!("Usage:");
+        println!("  rpass <number> <number>");
+        return;
+    }
+
+    let y = ar.pop().unwrap().parse::<u32>();
+    let x = ar.pop().unwrap().parse::<u32>();
+
+    let rx = match x {
+        Ok(v)   => v,
+        Err(..) => panic!("You need to pass an integer"),
+    };
+
+    let ry = match y {
+        Ok(v)   => v,
+        Err(..) => panic!("You need to pass an integer"),
+    };
+
+    print!("{}", random_password(rx, ry));
 }
