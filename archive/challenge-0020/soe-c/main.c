76558cf71eb4006230b250d068e396ccc27e986c
psyomn <lethaljellybean@gmail.com>
2016-02-28 20:04:03 -0500

    challenge 20: c: add more test data for main.c

--- archive/challenge-0020/soe-c/main.c
+++ archive/challenge-0020/soe-c/main.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
 #include "soe.h"
 
 int main(int argc, char * argv[]) {
-  resultset_t* rs = soe(100);
+  resultset_t* rs;
+
+  printf("\n");;
+  rs = soe(100);
   print_soe(rs);
   free(rs);
+
+  printf("\n");;
+  rs = soe(500);
+  print_soe(rs);
+  free(rs);
+
+  printf("\n");;
+  rs = soe(25000);
+  print_soe(rs);
+  free(rs);
+
   return 0;
 }
