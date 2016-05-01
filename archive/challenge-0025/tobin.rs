8840c46eef1a30e9afd459b342f3c286872f4553
Simon Symeonidis <lethaljellybean@gmail.com>
2016-04-30 23:04:58 -0400

    challenge 25: intermediate: rust

---
+++ archive/challenge-0025/tobin.rs
@@ -0,0 +1,14 @@
+use std::io;
+
+fn main() -> () {
+    let mut input = String::new();
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let number = input
+        .trim()
+        .parse::<i32>()
+        .unwrap();
+
+    println!("{:b}", number);
+}
